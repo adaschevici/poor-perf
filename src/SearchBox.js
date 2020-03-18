799f941d7c0bf844c8a957114201e3647394c706
Artur Daschevici <a.daschevici@gmail.com>
2020-03-18 11:24:56 +0000

    Improved performance with react virtualized

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,36 +1,87 @@
 import React, { Component } from 'react'
+import styles from './SearchBox.css'
 import { books } from './titles.json'
+import { List } from 'react-virtualized'
+
+class ReactVirtualizedList extends Component {
+  static defaultProps = {
+    searchResults: [],
+  }
+  constructor(props) {
+    super(props)
+    this.listHeight = 600
+    this.rowHeight = 50
+    this.rowWidth = 800
+  }
+
+  renderRow = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style} className={styles.row}>
+        <div className={styles.content}>
+          <div>{this.props.searchResults[index].id}</div>
+          <div>{this.props.searchResults[index].originalTitle}</div>
+        </div>
+      </div>
+    )
+  }
+
+  render() {
+    const { rowHeight, rowWidth, listHeight } = this
+    const { searchResults } = this.props
+    return (
+      <List
+        width={rowWidth}
+        height={listHeight}
+        rowHeight={rowHeight}
+        rowRenderer={this.renderRow}
+        rowCount={searchResults.length}
+        overscanRowCount={3}
+      />
+    )
+  }
+}
+
+class SearchResults extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchResults: books,
+    }
+  }
+  componentDidUpdate(prevProps) {
+    const { searchTerm } = this.props
+    const results = books.filter(book =>
+      book.originalTitle.includes(this.props.searchTerm)
+    )
+    if (searchTerm && searchTerm !== prevProps.searchTerm) {
+      this.setState({
+        searchResults: results,
+      })
+    }
+  }
+
+  render() {
+    return <ReactVirtualizedList searchResults={this.state.searchResults} />
+  }
+}
 
 export default class SearchBox extends Component {
   constructor(props) {
     super(props)
     this.state = {
       searchTerm: '',
-      searchResults: [],
     }
   }
 
-  getSearchResults = evt => {
-    evt.persist()
-    const searchResults = books.filter(book =>
-      book.originalTitle.includes(evt.target.value)
-    )
-    this.setState({
-      searchTerm: evt.target.value,
-      searchResults,
-    })
-  }
-
   render() {
-    const { searchResults, searchTerm } = this.state
     return (
       <div>
-        <input onChange={this.getSearchResults} value={searchTerm} />
-        <ul>
-          {searchResults.map(book => (
-            <li>{book.originalTitle}</li>
-          ))}
-        </ul>
+        <input
+          onChange={e => this.setState({ searchTerm: e.target.value })}
+          value={this.state.searchTerm}
+        />
+
+        <SearchResults searchTerm={this.state.searchTerm} />
       </div>
     )
   }
